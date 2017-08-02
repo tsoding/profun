07323a37c9299c5140fba5ba0e3894dcb3428cf3
rexim <reximkut@gmail.com>
2017-08-03 01:31:23 +0700

    Implement transformGame

--- functional/src/Logic.hs
+++ functional/src/Logic.hs
@@ -1,3 +1,18 @@
 module Logic where
 
+import Game
+import Graphics.Gloss.Interface.Pure.Game
+
+playerTurn :: Game -> (Int, Int) -> Game
+playerTurn game cellCoord = game
+
+mousePosAsCellCoord :: (Float, Float) -> (Int, Int)
+mousePosAsCellCoord (x, y) = ( floor ((y + (fromIntegral screenHeight * 0.5)) / cellHeight)
+                             , floor ((x + (fromIntegral screenWidth * 0.5)) / cellWidth)
+                             )
+
+transformGame (EventKey (MouseButton LeftButton) Up _ mousePos) game =
+    case gameState game of
+      Running -> playerTurn game $ mousePosAsCellCoord mousePos
+      GameOver _ -> initialGame
 transformGame _ game = game
